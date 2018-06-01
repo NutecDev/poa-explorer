b4ad7793877011c201e63c98bdf4650b49a9aef7
Amanda Sposito <amanda.sposito@plataformatec.com.br>
2018-06-01 19:12:34 -0300

    Add Smart Contract details when verified

--- apps/explorer_web/lib/explorer_web/views/address_contract_view.ex
+++ apps/explorer_web/lib/explorer_web/views/address_contract_view.ex
@@ -5,4 +5,9 @@ defmodule ExplorerWeb.AddressContractView do
 
   def smart_contract_verified?(%Address{smart_contract: nil}), do: false
   def smart_contract_verified?(%Address{smart_contract: %SmartContract{}}), do: true
+
+  def format_smart_contract_abi(abi), do: Poison.encode!(abi, pretty: true)
+
+  def format_optimization(true), do: gettext("true")
+  def format_optimization(false), do: gettext("false")
 end
